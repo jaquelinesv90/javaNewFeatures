d1bfa380e08b3ddfb42c7e2b028ef0b74756f608
Jaqueline <jaquelinesv90@gmail.com>
2022-09-11 07:17:19 -0700

    switch - statement example ava 13

---
+++ src/com/newFeatures/java13/switch_statement/Day.java
@@ -0,0 +1,12 @@
+package com.newFeatures.java13.switch_statement;
+
+public enum Day {
+	SUNDAY,
+	MONDAY,
+	TUESDAY,
+	WEDNESDAY,
+	THURSDAY,
+	FRIDAY,
+	SATURDAY;
+
+}
---
+++ src/com/newFeatures/java13/switch_statement/SwitchStatementExample.java
@@ -0,0 +1,43 @@
+package com.newFeatures.java13.switch_statement;
+
+public class SwitchStatementExample {
+	
+	
+	public static void main(String[] args) {
+		int numLettersBefore = 0 ;
+		Day dayBefore = Day.WEDNESDAY;
+		
+		switch(dayBefore) {
+		case MONDAY:
+		case FRIDAY:
+		case SUNDAY:
+			numLettersBefore=6;
+			break;
+		case TUESDAY:
+			numLettersBefore =7;
+			break;
+		case THURSDAY:
+		case SATURDAY:
+			numLettersBefore =7;
+		case WEDNESDAY:
+			numLettersBefore =9;
+			break;
+			default:
+				throw new IllegalStateException("Invalid day:"+ dayBefore);
+		}
+		
+		
+		// JAVA 13
+		int numLettersAfter = 0;
+		Day dayAfter = Day.WEDNESDAY;
+		switch(dayAfter) {
+			case MONDAY,FRIDAY,SUNDAY -> numLettersAfter = 6;
+			case TUESDAY			  -> numLettersAfter = 7;
+			case THURSDAY,SATURDAY    -> numLettersAfter = 8;
+			case WEDNESDAY			  -> numLettersAfter = 9;
+			default -> throw new IllegalStateException("Invalid day:"+ dayBefore);
+		};
+		
+	}
+
+}
