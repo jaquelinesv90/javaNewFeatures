f624777b94a8baf977dad7152c42894facce2673
jaquelinesv90 <jaquelinesv90@gmail.com>
2022-10-23 06:17:35 -0700

    example of switch statement - java 14

--- src/com/newFeatures/java14/switch_statement/SwitchStatementExample.java
+++ src/com/newFeatures/java14/switch_statement/SwitchStatementExample.java
@@ -1,5 +1,32 @@
 package com.newFeatures.java14.switch_statement;
 
 public class SwitchStatementExample {
-
+	
+	public static void main(String[] args) {
+		
+		boolean isTodayHolidayOld;
+		String day = "MONDAY";
+		
+		switch(day) {
+			case "MONDAY":
+			case "TUESDAY":
+			case "WEDNESDAY":
+			case "THURSDAY":
+			case "FRIDAY":
+				isTodayHolidayOld = false;
+				break;
+			case "SATURDAY":
+			case "SUNDAY":
+				isTodayHolidayOld = true;
+				break;
+			default:
+				throw new IllegalArgumentException("what's a "+ day);
+		}
+		
+		boolean isTodayHolidayNew = switch(day) {
+			case "MONDAY","TUESDAY","WEDNESDAY","THURSDAY","FRIDAY" -> false;
+			case "SATURDAY","SUNDAY" -> true;
+			default -> throw new IllegalArgumentException("what's a "+ day);
+		};
+	}
 }
